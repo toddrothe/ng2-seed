1d127613eb3f9654b531f07728207ebcc80bf773
toddrothe <todd@toddrothe.com>
2016-01-27 09:53:03 -0800

    initial working seed with Angular2, typescript, gulp and browser-sync

---
+++ app/app.component.ts
@@ -0,0 +1,12 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'hg-tool',
+    directives:[],
+    providers:[],
+    template: `
+    <h1>Welcome</h1>
+    <button class="ui button"> Follow </button>
+    `
+})
+export class AppComponent { }
