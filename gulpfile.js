a0af21c03d32d734430348ddfc2bd843c24d1e28
toddrothe <todd@toddrothe.com>
2016-01-27 13:52:56 -0800

    add semantic ui to gulp tasks
    gulp fresh build - to build semnatic
    gulp - will trigger watch on semantic

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync');
 var reload = browserSync.reload;
+var semanticWatch = require('./semantic/tasks/watch');
+var semanticBuild = require('./semantic/tasks/build');
 
 gulp.task('sass', function () {
   gulp.src('./scss/**/*.scss')
@@ -10,13 +12,18 @@ gulp.task('sass', function () {
     .pipe(browserSync.stream());
 });
 
+gulp.task('build semantic', semanticBuild);
+gulp.task('watch semantic', semanticWatch);
+
 gulp.task('serve', function() {
   browserSync({
     server: {
       baseDir: '.'
     }
   });
-  gulp.watch( ['scss/*.scss','*.html', 'app/**/*.js'], ['sass'], {cwd: '.'}, reload);
+  gulp.watch( ['scss/*.scss','*.html', 'app/**/*.js'], ['sass', 'watch semantic'], {cwd: '.'}, reload);
 });
+
+gulp.task('fresh build', ['sass','serve', 'build semantic']);
 
 gulp.task('default', ['sass','serve']);
