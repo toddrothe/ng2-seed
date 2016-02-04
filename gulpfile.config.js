d7e923f205ebdee95b146a989ec71d61348eb128
toddrothe <todd@toddrothe.com>
2016-02-03 22:03:45 -0800

    Add gulp.config file
    Missed this in the last checkin
    Update gitignore

---
+++ gulpfile.config.js
@@ -0,0 +1,17 @@
+'use strict';
+var GulpConfig = (function () {
+    function gulpConfig() {
+        //Got tired of scrolling through all the comments so removed them
+        //Don't hurt me AC :-)
+        this.source = '.';
+        this.sourceApp = this.source + '/src';
+
+        this.tsOutputPath = this.source + '/dist';
+        this.allTypeScript = this.sourceApp + '/**/*.ts';
+
+        this.typings = './typings/';
+        this.libraryTypeScriptDefinitions = './typings/**/*.ts';
+    }
+    return gulpConfig;
+})();
+module.exports = GulpConfig;
